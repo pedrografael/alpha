f31570b8f561f9d268519f20bf63051ca51b00a3
alpha <alpha@alpha-november>
2017-07-02 15:20:51 -0300

    move endpoint

--- src/main/java/com/github/pedrografael/endpoint/TestRestController.java
+++ src/main/java/com/github/pedrografael/endpoint/TestRestController.java
@@ -1,4 +1,4 @@
-package com.github.pedrografael.application.endpoint;
+package com.github.pedrografael.endpoint;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
